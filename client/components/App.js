f8be4abe0a7ab039bb605b7a2b2ced6aac76f247
Kanadust <liy063@ucsd.edu>
2018-04-18 23:48:30 -0700

    clean dump

--- client/components/App.js
+++ client/components/App.js
@@ -5,7 +5,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Helloaa aTeam Solid</h1>
+          <h1 className="App-title">Hello Team Solid</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
--- webpack.config.js
+++ webpack.config.js
@@ -3,36 +3,11 @@ const webpack = require('webpack');
 
 module.exports = {
   entry: './client/index.js',
-  // entry: {
-  //   app: [
-  //     'webpack-hot-middleware/client',
-  //     'react-hot-loader/patch',
-  //     './client/index.js',
-  //   ],
-  //   vendor: [
-  //     'react',
-  //     'react-dom',
-  //   ],
-  // },
   output: {
     path: path.join(__dirname, 'client'),
     filename: 'bundle.js',
     publicPath: '/'
   },
-  // plugins: [
-  //   new webpack.HotModuleReplacementPlugin(),
-  //   new webpack.optimize.CommonsChunkPlugin({
-  //     name: 'vendor',
-  //     minChunks: Infinity,
-  //     filename: 'vendor.js',
-  //   }),
-  //   new webpack.DefinePlugin({
-  //     'process.env': {
-  //       CLIENT: JSON.stringify(true),
-  //       'NODE_ENV': JSON.stringify('development'),
-  //     }
-  //   }),
-  // ],
   module: {
     loaders: [{
       test: /.jsx?$/,
