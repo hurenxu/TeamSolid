c1e8e770c2ca50bedcbb41d220c1d8bd8aa15131
Kanadust <liy063@ucsd.edu>
2018-04-18 23:02:36 -0700

    front-end 'hot' reload

--- server/server.js
+++ server/server.js
@@ -2,8 +2,20 @@ var express = require('express');
 var router = require('./routes/routes.js')
 var path = require('path');
 var app = express();
+
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const config = require('../webpack.config.js');
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../client'));
+
+const compiler = webpack(config);
+app.use(webpackDevMiddleware(compiler, {
+  publicPath: config.output.publicPath
+}));
+
 app.use(express.static(path.join(__dirname, '../client')));
 app.use('/', router);
+
 module.exports=app;
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,50 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
+
 module.exports = {
- entry: './client/index.js',
- output: {
-  path: path.join(__dirname, 'client'),
-  filename: 'bundle.js'
- },
- module: {
-  loaders: [{
-   test: /.jsx?$/,
-   loader: 'babel-loader',
-   exclude: /node_modules/,
-   query: {
-    presets: ['es2015', 'react']
-   }
+  entry: './client/index.js',
+  // entry: {
+  //   app: [
+  //     'webpack-hot-middleware/client',
+  //     'react-hot-loader/patch',
+  //     './client/index.js',
+  //   ],
+  //   vendor: [
+  //     'react',
+  //     'react-dom',
+  //   ],
+  // },
+  output: {
+    path: path.join(__dirname, 'client'),
+    filename: 'bundle.js',
+    publicPath: '/'
   },
-  {
-   test: /\.css$/,
-   loader: "style-loader!css-loader"
-  }]
- }
+  // plugins: [
+  //   new webpack.HotModuleReplacementPlugin(),
+  //   new webpack.optimize.CommonsChunkPlugin({
+  //     name: 'vendor',
+  //     minChunks: Infinity,
+  //     filename: 'vendor.js',
+  //   }),
+  //   new webpack.DefinePlugin({
+  //     'process.env': {
+  //       CLIENT: JSON.stringify(true),
+  //       'NODE_ENV': JSON.stringify('development'),
+  //     }
+  //   }),
+  // ],
+  module: {
+    loaders: [{
+      test: /.jsx?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+      query: {
+        presets: ['es2015', 'react']
+      }
+    },
+    {
+      test: /\.css$/,
+      loader: "style-loader!css-loader"
+    }]
+  }
 }
