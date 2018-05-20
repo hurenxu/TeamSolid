ae708fbc9f1a2007c9ccf82d7e2f977dcbd11031
Kanadust <liy063@ucsd.edu>
2018-05-20 00:28:18 -0700

    simple authorization

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,18 +1,96 @@
 var express = require('express');
 var router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
-
+var passport = require('passport')
+var LocalStrategy = require('passport-local').Strategy;
 // Connection URL
 const url = 'mongodb://localhost';
 
 // Database Name
 const dbName = 'team_solid_project';
 
+passport.serializeUser(function (user, done) {
+    console.log(user)
+
+    done(null, user._id);
+});
+
+passport.deserializeUser(function (id, done) {
+    MongoClient.connect(url, function (err, client) {
+        const db = client.db(dbName);
+
+        const col = db.collection("accounts");
+
+        col.findOne({ _id: id }, function (err, user) {
+            console.log(user)
+            console.log("haha")
+            done(err, user);
+        });
+    })
+});
+
+passport.use(new LocalStrategy(
+    function (username, password, done) {
+        console.log(username)
+        MongoClient.connect(url, function (err, client) {
+            const db = client.db(dbName);
+
+            const col = db.collection("accounts");
+
+            col.findOne({ username: username }, function (err, user) {
+                if (err) { return done(err); }
+                console.log(user)
+                if (!user) {
+                    return done(null, false, { message: 'Incorrect username.' });
+                }
+
+                if (user.password != password) {
+                    return done(null, false, { message: 'Incorrect password.' });
+                }
+
+                return done(null, user);
+            });
+        })
+    }
+));
+
 // index
 router.get('/', function (req, res) {
     res.render('index');
 });
 
+router.get('/loginpage', function (req, res) {
+    res.send('loginpage');
+});
+
+router.get('/profile',
+    require('connect-ensure-login').ensureLoggedIn(),
+    function (req, res) {
+        res.send('profile');
+    });
+
+router.post('/login',
+    passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/loginpage'
+    }));
+
+router.post('/register', function (req, res, next) {
+    var uname = req.body.username;
+    var pwd = req.body.password;
+
+    MongoClient.connect(url, function (err, client) {
+        const db = client.db(dbName);
+        var cl = db.collection('accounts');
+        var count;
+        cl.count(function (err, num) {
+            count = num;
+            cl.insertOne({ _id: (count + 1), username: uname, password: pwd }, function () {
+                console.log('insert!' + uname + ' ' + pwd);
+            });
+        });
+    });
+});
 
 // insert operation
 router.post('/insert', function (req, res, next) {
@@ -23,10 +101,10 @@ router.post('/insert', function (req, res, next) {
         const db = client.db(dbName);
         var cl = db.collection('userTable');
         var count;
-        cl.count(function(err, num) {
-            count=num;
-            cl.insertOne({ _id:(count+1), userEmail: email, password: passwd}, function () {
-                cl.find({}).toArray(function(err,list){
+        cl.count(function (err, num) {
+            count = num;
+            cl.insertOne({ _id: (count + 1), userEmail: email, password: passwd }, function () {
+                cl.find({}).toArray(function (err, list) {
                     if (err) throw err;
                     res.send(JSON.stringify(list));
                 });
@@ -42,7 +120,7 @@ router.get('/read', function (req, res) {
     MongoClient.connect(url, function (err, client) {
         const db = client.db(dbName);
 
-        db.collection('userName').count(function(err, count) {
+        db.collection('userName').count(function (err, count) {
             console.log(count)
         })
     });
