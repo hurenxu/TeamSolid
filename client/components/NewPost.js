0ea599f7800986462a8dfb228598b2d5b0e5c536
hurenxu <reh011@ucsd.edu>
2018-05-25 14:22:39 -0700

    adding the popups for the post

--- client/components/NewPost.js
+++ client/components/NewPost.js
@@ -1,20 +1,37 @@
-import React from 'react'
-import { Button, Header, Image, Modal, Form } from 'semantic-ui-react'
+import React, {Component} from 'react'
+import { Button, Header, Image, Modal, Form, TextArea } from 'semantic-ui-react'
+import PostPopup from './PostPopup'
 
 const styles = {
   width: '33vw',
   marginTop: '5vh',
 }
 
-const NewPost = () => (
-  <Form style={styles} reply>
-    <Form.TextArea />
-      <Button.Group>
-          <Button>Clear</Button>
-          <Button.Or />
-          <Button positive>Post</Button>
-      </Button.Group>
-  </Form>
-)
+class NewPost extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state =
+            {
+                open: 0
+            };
+    }
+
+    render() {
+        return (
+            <div>
+            <Form style={styles} reply>
+                <TextArea  placeholder='Click to Post' style={{ minHeight: 100 }} onClick={()=>this.setState({open: 1})}/>
+                <Button.Group>
+                    <Button>Clear</Button>
+                    <Button.Or />
+                    <Button positive>Post</Button>
+                </Button.Group>
+            </Form>
+            <PostPopup open={this.state.open === 1} onClose={()=> this.setState({open: 0})}/>
+            </div>
+        )
+    }
+}
 
 export default NewPost
