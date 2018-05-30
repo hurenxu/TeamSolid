1078c933469218c79560634e9755f1d412af25b9
Wanhui Qiao <qiaowanhui@gmail.com>
2018-05-30 02:26:33 -0700

    fix

--- client/components/FriendManagement.js
+++ client/components/FriendManagement.js
@@ -14,23 +14,48 @@ class FriendManagement extends Component {
     super(props);
     this.state = {
       targetFriend: "",
+      value: ""
     }
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value,
+                   targetFriend: event.target.value
+    });
   }
 
   handleSubmit(event) {
     console.log("Submit");
     console.log(this.state.targetFriend)
-    axios.post('/api/updateFriendList', {email: "8888@8888.com", tid: this.state.targetFriend, actionType: "add"}).then((response) => {
-      console.log(JSON.stringify(response))
+    axios.post('/api/searchUser', {searchKey: this.state.targetFriend}).then((response) => {
+      const checkResult = response.data;
+      if(checkResult){
+        console.log("It exists")
+        axios.post('/api/updateFriendList', {email: "8888@8888.com", tid: this.state.targetFriend, actionType: "add"}).then((response) => {
+          console.log(JSON.stringify(response.data));
+          alert('Success');
+          this.setState({value: ""});
+        });
+      }
+      else{
+        console.log("It doesn't exist")
+
+        alert('User doesn\'t exist!');
+        this.setState({value: ""});
+      }
     });
   }
 
   render(){
+
     return (
       <div style={style}>
-        <Input icon='users' iconPosition='left' onChange={(e, {value})=> this.setState({targetFriend: value})} placeholder='Search users...' />
+        <Input icon='users' iconPosition='left' value={this.state.value}
+               onChange={this.handleChange} placeholder='Search users...' />
         <Button icon labelPosition='right' onClick={this.handleSubmit}>
+          Add Friend
           <Icon name='right arrow' />
         </Button>
       </div>
