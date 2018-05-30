6c5836d0206e831f45edab55fa5bd4ceeb3607a9
Wanhui Qiao <qiaowanhui@gmail.com>
2018-05-30 00:36:04 -0700

    Add friend
    ff

--- client/components/FriendManagement.js
+++ client/components/FriendManagement.js
@@ -1,43 +1,21 @@
 import React, {Component} from 'react';
+import {Grid, Ref} from 'semantic-ui-react'
+import Responsive from 'react-responsive';
 import axios from 'axios';
-import ReactDOM from "react-dom";
-import { Input, Grid, Button} from 'semantic-ui-react'
+import { Container, Form, Popup, Button, Header, Image, Modal } from 'semantic-ui-react'
 
-const FriendManamentStyle = {
-  marginTop: '3em'
-}
-
-export default class FriendManagement extends Component {
-  componentWillMount() {
-    this.resetComponent()
-  }
-
-  resetComponent = () => this.setState({ isLoading: false, results: [], value: '' })
-
-  handleResultSelect = (e, { result }) => this.setState({ value: result.title })
-
-  handleSearchChange = (e, { value }) => {
-    this.setState({ isLoading: true, value })
-
-    setTimeout(() => {
-      if (this.state.value.length < 1) return this.resetComponent()
-
-      const re = new RegExp(_.escapeRegExp(this.state.value), 'i')
-      const isMatch = result => re.test(result.title)
-
-      this.setState({
-        isLoading: false,
-        results: _.filter(source, isMatch),
-      })
-    }, 300)
-  }
-
-  render() {
+class FriendManagement extends Component {
+  render(){
+    return (
+      <div>
+        <Input icon='users' iconPosition='left' placeholder='Search users...' />
+        <Button icon labelPosition='right'>
+          Next
+          <Icon name='right arrow' />
+        </Button>
+      </div>
+    );
   }
 }
 
-  render() {<Grid style={FriendManagement}>
-  <Input icon='users' iconPosition='left' placeholder='Search users...' />
-    <Button>Click Here</Button>
-  </Grid>}
-}
+export default FriendManagement
