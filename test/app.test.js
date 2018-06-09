87307679c311c428eeb1243b521fb94aca47b50b
lyfkfc <yil385@ucsd.edu>
2018-06-08 23:07:06 -0700

    update packages and babelrc to make sure test ok

---
+++ test/app.test.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+//import chai from 'chai';
+import {expect} from 'chai';
+import { mount, shallow } from 'enzyme';
+const sinon = require('sinon');
+
+import App from '../client/components/App';
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('App Component', () => {
+    it('should have a header inside the app', () => {
+        const wrapper = shallow(<App/>);
+        expect(wrapper.find('header')).to.have.length(1);
+    });
+
+    it('should have a title inside the app', () => {
+        const wrapper = shallow(<App/>);
+        expect(wrapper.find('h1')).to.have.length(1);
+    });
+});
